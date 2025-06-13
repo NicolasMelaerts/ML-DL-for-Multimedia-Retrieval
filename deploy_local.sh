ba243ce7e4f7cf0c8688a3bc2166db3f3648ea1e
Nicolas Melaerts <82506949+NicolasMelaerts@users.noreply.github.com>
2025-06-13 11:47:51 +0200

    Update deploy_local.sh

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker compose down      # Arrête et supprime les conteneurs
 docker compose rm -f     # Force la suppression des conteneurs arrêtés
 docker compose build     # Recrée les images Docker
